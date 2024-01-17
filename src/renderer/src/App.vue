955051bb097909f6229a9bdc6b74fd0d899078c3
CrossDev <joancruz0502@hotmail.com>
2024-01-17 06:59:42 -0400

    feat: create ui

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AppLayout from './layouts/AppLayout.vue'
+</script>
+<template>
+	<DraggableTopbar />
+	<AppLayout flex="~ row">
+		<SideBar p2 bg-zinc-900:30>
+			<ActionButtonsrow mt10 h-auto w-auto flex justify-between mt1 />
+			<NotePreviewList />
+		</SideBar>
+		<MainContent p2 border-l bg-zinc-900:50 border-l-white:20>
+			
+		</MainContent>
+	</AppLayout>
+</template>
+<style scope lang="less">
+@import './assets/css/styles.less';
+</style>
