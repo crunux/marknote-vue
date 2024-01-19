25bea0522bb80f5da5431b74fc5ffed2562e8552
CrossDev <joancruz0502@hotmail.com>
2024-01-19 01:46:14 -0400

    feat: add pinia

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import 'virtual:uno.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.mount('#app')
