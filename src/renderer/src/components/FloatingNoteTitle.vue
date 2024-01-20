43967dfb2a3fcb8d32c08c70f73f2437954b5629
CrossDev <joancruz0502@hotmail.com>
2024-01-20 18:14:09 -0400

    feat: add title from state

--- src/renderer/src/components/FloatingNoteTitle.vue
+++ src/renderer/src/components/FloatingNoteTitle.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-const title = ref('Note Title')
+import { useStoreNotes } from '../store/index'
+
+const store = useStoreNotes()
+
 </script>
 <template>
-	<div flex justify-center >
-		<span text-gray-400>{{title}}</span>
+	<div flex justify-center>
+		<span text-gray-400>{{ store.note?.title }}</span>
 	</div>
 </template>
 <style scoped></style>
