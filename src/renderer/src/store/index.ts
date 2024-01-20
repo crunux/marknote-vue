6adb2fd55f7d42c89a91da5d3f02a9decae8a5ca
CrossDev <joancruz0502@hotmail.com>
2024-01-20 18:11:54 -0400

    refactor: reactive state

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,35 +1,34 @@
 import type { NoteInfo } from '@shared/types'
 import { defineStore } from 'pinia'
-import {ref, computed} from 'vue'
+import { ref, computed } from 'vue'
 import { notesMock } from './mock'
 
-
-
-export const useStoreNotes = defineStore('notesAtom', () => {
+export const useStoreNotes = defineStore('notesStore', () => {
 	const notesAtom = ref<NoteInfo[]>(notesMock)
 	const selectedNoteIndex = ref<number | null>(null)
+	const note = ref<NoteInfo | null>(null)
+	const content = ref<string>(`# Welcome`)
 
-	const selectNote = () => {
-		const notes = computed(() => notesAtom.value)
-		const selectedNote = computed(() => selectedNoteIndex.value)
-
-		if (!selectedNote.value) return
-
-		const note = notes.value[selectedNote.value]
-
+	const selectNote = (index?: number) => {
+		selectedNoteIndex.value = index!
+		note.value = notesAtom.value[selectedNoteIndex.value]
+		content.value = `# Welcome from Note${selectedNoteIndex.value}`
 		return {
-			...note,
-
-			content: `Hello From Note ${selectedNote.value}`,
-			index: selectedNote.value
+			...note.value,
+			index: selectedNoteIndex.value,
+			content: content.value
 		}
 	}
 
 	return {
 		//variable
-		notesAtom,
-		selectedNote: selectedNoteIndex.value,
+		note,
+		content,
 
+		notesAtom,
+		selectedNoteIndex,
+		//getters
+		noteComputed: computed(() => note.value),
 		//setter
 		selectNote
 	}
